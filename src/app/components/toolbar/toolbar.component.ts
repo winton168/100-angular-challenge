69ebd938ee59340489cc05f9b14cc35bee3991f9
Winton Wang <wwang@cmigroup.com>
2021-06-12 20:43:27 -0400

    initial

---
+++ src/app/components/toolbar/toolbar.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toolbar',
+  templateUrl: './toolbar.component.html',
+  styleUrls: ['./toolbar.component.scss']
+})
+export class ToolbarComponent implements OnInit {
+
+  @Input() title: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
